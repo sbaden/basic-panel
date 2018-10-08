57a3430b521de4bc8cbe14edaf2ffb659724030e
SBaden <sbaden@design01-ladm.us.nfl.net>
2018-10-08 13:03:55 -0700

    Update HTML, JS & Manifest

--- js/main.js
+++ js/main.js
@@ -2,15 +2,16 @@ $(document).ready(function(){
     'use strict';
     alert('JS connected');
 
-    var csInterface = new CSInterface();
+    let csInterface = new CSInterface();
 
 
     window.onload = function(){
-        // PASS OBJECT TO JSX FOR EVALUATION
-        var profileObj = {
+        
+        let profileObj = {
             message: "ExendScript connected"
         }
-
+        
+        // PASS OBJECT TO JSX FOR EVALUATION
         csInterface.evalScript('parseObj(' + JSON.stringify(profileObj) + ')');
 
 
@@ -30,12 +31,12 @@ $(document).ready(function(){
             location.reload();
 
             //// RELOAD JSX - NOT WORKING ////
-            // var appName = csInterface.hostEnvironment.appName;
-            // var extensionPath = csInterface.getSystemPath(SystemPath.EXTENSION);
-            // var extensionRootGeneral = extensionPath + '/jsx/';  // load general JSX script independent of appName
+            // let appName = csInterface.hostEnvironment.appName;
+            // let extensionPath = csInterface.getSystemPath(SystemPath.EXTENSION);
+            // let extensionRootGeneral = extensionPath + '/jsx/';  // load general JSX script independent of appName
             // csInterface.evalScript('evalFiles("' + extensionRootGeneral + '")');
         
-            // var extensionRootApp = extensionPath + '/jsx/' + appName + '/';  // load JSX scripts based on appName
+            // let extensionRootApp = extensionPath + '/jsx/' + appName + '/';  // load JSX scripts based on appName
             // csInterface.evalScript('evalFiles("' + extensionRootApp + '")');
         });
     }
